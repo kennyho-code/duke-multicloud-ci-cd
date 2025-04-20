b4416b303fc84bcf644c0674e6fab2dfa0563e62
Kenny Ho <kennyho.code@gmail.com>
2025-04-20 13:45:27 -0400

    add function

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-
+from hello import hello
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    result = hello()
+    return result
 
 
 if __name__ == "__main__":
